55dccb9e65e6f35bfb2a3f31db6b88952ca9ec47
MauriAL10 <mau.27.02@gmail.com>
2021-04-12 23:52:35 -0400

    WEB API para registro de estudiantes creada

---
+++ Practica2/Controllers/StudentsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica2.Controllers
+{
+    [ApiController]
+    [Route("/api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        public StudentsController()
+        {
+
+        }
+
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            List<Student> lista = new List<Student>();
+            lista.Add(new Student { Name = "Mauricio Angulo" });
+            lista.Add(new Student { Name = "Mayra Oropeza" });
+            lista.Add(new Student { Name = "Camila Medina" });
+            lista.Add(new Student { Name = "Fabricio Fernandez" });
+            lista.Add(new Student { Name = "Carlos Coronado" });
+            lista.Add(new Student { Name = "Nicolas Barbery" });
+            return lista;
+
+        }
+
+        [HttpPost]
+        public Student CreateStudent([FromBody] string studentName)
+        {
+            return new Student()
+            {
+                Name = studentName
+            };
+        }
+
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student student)
+        {
+            student.Name = "updated";
+            return student;
+        }
+        [HttpDelete]
+        public Student DeleteStudent([FromBody] Student student)
+        {
+            student.Name = "deleted";
+            return student;
+        }
+    }
+}
